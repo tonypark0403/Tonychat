4e93bb5fd8ef2a8288c52227bfc2a66f5be8b9ed
Tony Park <c3dream@gmail.com>
2017-10-27 01:09:25 -0400

    implemented server parameters and connection

--- app/src/main/java/com/example/tonypark/tonychat/LoadActivity.java
+++ app/src/main/java/com/example/tonypark/tonychat/LoadActivity.java
@@ -1,13 +1,78 @@
 package com.example.tonypark.tonychat;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
 public class LoadActivity extends AppCompatActivity {
+
+    private LinearLayout linearLayout;
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load);
+
+        linearLayout = (LinearLayout) findViewById(R.id.loadactivity_linearlayout);
+
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setDeveloperModeEnabled(BuildConfig.DEBUG)
+                .build();
+        mFirebaseRemoteConfig.setConfigSettings(configSettings);
+        mFirebaseRemoteConfig.setDefaults(R.xml.default_config);
+
+        // cacheExpirationSeconds is set to cacheExpiration here, indicating the next fetch request
+        // will use fetch data from the Remote Config service, rather than cached parameter values,
+        // if cached parameter values are more than cacheExpiration seconds old.
+        // See Best Practices in the README for more information.
+        mFirebaseRemoteConfig.fetch(0) //3600 is 1 hour
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(LoadActivity.this, "Fetch Succeeded",
+                                    Toast.LENGTH_SHORT).show();
+
+                            // After config data is successfully fetched, it must be activated before newly fetched
+                            // values are returned.
+                            mFirebaseRemoteConfig.activateFetched();
+                        } else {
+                            Toast.makeText(LoadActivity.this, "Fetch Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        displayMessage();
+                    }
+                });
+    }
+
+    private void displayMessage() {
+        String load_background = mFirebaseRemoteConfig.getString("load_background");
+        boolean caps = mFirebaseRemoteConfig.getBoolean("load_message_caps");
+        String load_message = mFirebaseRemoteConfig.getString("load_message");
+
+        linearLayout.setBackgroundColor(Color.parseColor(load_background));
+
+        if(caps) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(load_message).setPositiveButton("Check", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    finish();
+                }
+            });
+            builder.create().show();
+        }
     }
 }
